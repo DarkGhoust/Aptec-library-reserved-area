8ac07bdd4c0592245f3a3d53a43e296426dec6f2
DarkGhoust <DarkGhoust10@gmail.com>
2023-03-02 16:36:56 +0000

    Js prettify

--- js/hide_evaluated.php
+++ js/hide_evaluated.php
@@ -1,10 +1,10 @@
 <script>
-jQuery('tr[data-revisor="<? echo $this->name;?>"').each( function(){
-	const id = parseInt( jQuery(this).find('td')[0].innerHTML )
-	jQuery(`tr[data-id="${id}"]`).addClass('reviewed')
-})
+	jQuery('tr[data-revisor="<? echo $this->name; ?>"').each( function(){
+		const id = parseInt( jQuery(this).find('td')[0].innerHTML )
+		jQuery(`tr[data-id="${id}"]`).addClass('reviewed')
+	})
 
-jQuery('.toggle-reviewed').click( function(e){
-    jQuery('.reviewed').toggle()
-})
+	jQuery('.toggle-reviewed').click( function(e){
+		jQuery('.reviewed').toggle()
+	})
 </script>
--- js/submissions.js
+++
@@ -1,3 +0,0 @@
-jQuery('').click( function(e){
-
-})
